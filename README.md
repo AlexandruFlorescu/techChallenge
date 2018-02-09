ce6a88f73a1d275a413ee218c030b6f830fd96d4
FlowerDts <acdc152@gmail.com>
2018-02-09 16:19:21 +0200

    final touches

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
-# techChallenge
+# Technical Challenge
 
 This is a Python script that is attempting to group documents in this dataset: http://csmining.org/index.php/webkb.html 
 
-The program uses a TF*IDF approach to rate each word in the documents. It thus uses these rating as features in order to train a scikit provided Kmeans and Decision Trees models. The second one is clearly better than the Kmeans, but we can't accurately score the Kmeans due to the labels having different meaning.
+The program uses a TF*IDF approach to rate each word in the documents. It thus uses these rating as features in order to train a scikit provided Kmeans, Logistic Regression and Decision Trees models.
 
-# TODO
+# Results
+The results are as follow:
+Logistic Regression:
+	Accuracy on the training subset: 0.995
+	Accuracy on the test subset: 0.872
+Decision Trees:
+	Accuracy on the training subset: 0.995
+	Accuracy on the test subset: 0.753
+KMeans (4):
+	Accuracy on the training subset: 0.352
 
-1. Implement Decision Trees
-2. Implement Kmeans
-3. Look into how to measure Kmeans score
+
+The script is running all this algorithms sequencially. To run it simply type: python solution.py
+
+PS: the solution uses python 2.7 in order to run. I think the only change required for 3.5 is adding () to the print function.
--- solution.py
+++ solution.py
@@ -4,18 +4,25 @@ from sklearn import metrics
 from sklearn.cluster import KMeans
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import preprocessing
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn import decomposition
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics.cluster import normalized_mutual_info_score
+from sklearn import mixture
 import numpy as np
 import sys
 
 
 def label_encoding(dt):
+	''' encoding string categorical into numerical categorical '''
 	lenc = preprocessing.LabelEncoder()
 	for i in range(len(dt)):
 		dt = lenc.fit_transform(dt)
 	return dt
 
 def read_data():
+	''' reads input train and test from documents '''
 	with open('train.txt') as documents:
 		train_dataset = documents.readlines()
 		train_dataset_split = [doc.split() for doc in train_dataset]
@@ -29,6 +36,7 @@ def read_data():
 	return train_dataset, label_encoding(train_labels), test_dataset, label_encoding(test_labels)
 
 def tf(term, doc):
+	''' computes tf on the term and the document '''
 	count = 0
 	for word in doc:
 		if word == term:
@@ -36,6 +44,7 @@ def tf(term, doc):
 	return count / len(doc)
 
 def idf(term, docs):
+	''' computes idf on the term and the documents given. Laplace smoothing can be added '''
 	nr_docs = len(docs) #+ 1 #laplace smoothing 
 	nr_docs_with_term = 0 #+ 1 #laplace smoothing 
 	for doc in docs:
@@ -44,36 +53,47 @@ def idf(term, docs):
 	return log(nr_docs/nr_docs_with_term)
 
 def doc_to_vec(doc, idfs, vocab):
+	''' converts given document to features vector '''
 	feats = np.zeros(len(vocab))
 	for word in doc:
 		if word in vocab: # filter words that only appear in test_dataset
 			feats[vocab.index(word)] = tf(word, doc) * idfs[word]
 	return feats
 
+def split_by_class(dataset, labels):
+	''' Unused! split dataset by classes. '''
+	classes_dataset = [[], [], [], []]
+	for index, row in enumerate(dataset):
+		classes_dataset[labels[index]].append(row)
+	return classes_dataset
+
+
 if __name__ == '__main__':
+
 	train_dataset, train_labels, test_dataset, test_labels = read_data()
 	print 'Training dataset has: {} documents'.format(len(train_dataset))
 	print 'Testing dataset has: {} documents'.format(len(test_dataset))
+	
 	print 'Building vocabularies...',
 	sys.stdout.flush()
 	train_vocab = set([])
-	test_vocab = set([])
+	# test_vocab = set([])
 	for doc in train_dataset:
 		train_vocab = train_vocab.union(set(doc))
-	for doc in test_dataset:
-		test_vocab = test_vocab.union(set(doc))
+	# for doc in test_dataset:
+	# 	test_vocab = test_vocab.union(set(doc))
 	print 'Done'
 
 	print 'Computing idfs on vocabularies...',
 	sys.stdout.flush()
 	train_idfs = {}
-	test_idfs = {}
+	# test_idfs = {}
 	for word in train_vocab:
 		if word not in train_idfs:
 			train_idfs[word] = idf(word, train_dataset)
-	for word in test_vocab:
-		if word not in test_idfs:
-			test_idfs[word] = idf(word, test_dataset)
+	# for word in test_vocab:
+	# 	if word not in test_idfs:
+	# 		test_idfs[word] = idf(word, test_dataset)
 	print 'Done'
 
 	print 'Converting documents to numerical features...',
@@ -82,13 +102,43 @@ if __name__ == '__main__':
 	test_vectorized = [doc_to_vec(doc, train_idfs, list(train_vocab)) for doc in test_dataset]
 	print 'Done'
 
-	print 'Clustering using KMeans algorithm...',
+	print 'Scaling the data using MinMaxScaler...',
 	sys.stdout.flush()
-	kmeans = KMeans(n_clusters = 7,random_state = 0).fit(train_vectorized)
+	scaler = MinMaxScaler()
+	train_vectorized = scaler.fit_transform(train_vectorized)
+	test_vectorized = scaler.fit_transform(test_vectorized)
 	print 'Done'
-	print 'KMeans(7): Accuracy on the training subset: {:.3f}'.format(metrics.adjusted_mutual_info_score(train_labels, kmeans.labels_))#kmeans.score(train_vectorized, train_labels))
+
+	# turns out the same improvement on Kmeans can be obtained using normalization applied below
+
+	# print 'Applying Principal Component Analysis...',
 	# sys.stdout.flush()
-	# print 'KMeans(7): Accuracy on the test subset: {:.3f}'.format(metrics.adjusted_mutual_info_score(kmeans.fit(test_vectorized).score(test_vectorized, test_labels))
+	# pca = decomposition.PCA(n_components = 3)
+	# train_vectorized = pca.fit_transform(train_vectorized)
+	# test_vectorized = pca.fit_transform(test_vectorized)
+	# print 'Done'
+
+
+	# print 'Trying the GaussianMixture trick that Tibi suggested',
+	# sys.stdout.flush()
+	# gmm = mixture.BayesianGaussianMixture(covariance_type = 'spherical').fit(train_vectorized, train_labels)
+	# # gmms = []
+	# # data_by_class = split_by_class(train_vectorized, train_labels)
+	# # for index,cs in enumerate(data_by_class[:1]):
+	# # 	gmms.append(mixture.BayesianGaussianMixture( covariance_type='spherical'))
+	# # 	gmms[index].fit(cs)
+	# for i in range(len(test_labels)):
+	# 	print test_labels[i], gmm.predict_proba(test_vectorized[i].reshape(1,-1))
+	# print 'Done'
+
+	print 'Classifying using logistic regression...',
+	sys.stdout.flush()
+	logreg = LogisticRegression(C=1e5).fit(train_vectorized, train_labels)
+	print 'Done'
+	print 'Accuracy on the training subset: {:.3f}'.format(logreg.score(train_vectorized, train_labels))
+	sys.stdout.flush()
+	print 'Accuracy on the test subset: {:.3f}'.format(logreg.score(test_vectorized, test_labels))
+
 
 	print 'Clustering using Decision Trees algorithm...',
 	sys.stdout.flush()
@@ -99,9 +149,13 @@ if __name__ == '__main__':
 	sys.stdout.flush()
 	print 'DTree: Accuracy on the test subset: {:.3f}'.format(tree.score(test_vectorized, test_labels))
 
+	print 'Clustering using KMeans algorithm...',
+	sys.stdout.flush()
+	train_vectorized = preprocessing.normalize(train_vectorized)
 
-	# np.set_printoptions(threshold=np.nan)
-	# print kmeans.labels_, train_labels
-	# print normalized_mutual_info_score(kmeans.labels_, train_labels)
+	kmeans = KMeans(n_clusters = 4,random_state = 0).fit(train_vectorized)
+	print 'Done'
+	# np.set_printoptions(threshold=np.nan) this allows to see all the values in the labels array
+	print 'KMeans(4): Accuracy on the training subset: {:.3f}'.format(metrics.adjusted_mutual_info_score(train_labels, kmeans.labels_))#kmeans.score(train_vectorized, train_labels))
 
-	# print len(train_dataset[0]), len(doc_to_vec(train_dataset[0], idfs))
+		
